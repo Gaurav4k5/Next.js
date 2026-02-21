e1e1f67aed8b952de40b29db87b3ddd86d9fe521
Gaurav <teachnicalgaurav45@gmail.com>
2026-02-21 09:04:28 +0530

    next with me

---
+++ video 126 Middleware/my-app/middleware.js
@@ -0,0 +1,22 @@
+// import { NextResponse } from 'next/server'
+ 
+// // This function can be marked `async` if using `await` inside
+// export function middleware(request) {
+//     return NextResponse.json({message: 'Hello from the about page'})
+//   return NextResponse.redirect(new URL('/home', request.url))
+// }
+ 
+// // See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: '/about/:path*',
+// }
+
+
+import { NextResponse } from 'next/server'
+ 
+export function middleware(request) {
+  if (request.nextUrl.pathname === '/about') {
+    request.nextUrl.pathname = '/about2'
+    return NextResponse.redirect(request.nextUrl)
+  }
+}
